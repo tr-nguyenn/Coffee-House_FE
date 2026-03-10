31e8d32eebfd88f2bb485cf4967b92407a16dbc3
ADMIN <ADMIN@NGUYENNTTD01427>
2026-03-10 23:06:42 +0700

    install bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import {createApp} from "vue";
+import App from "./App.vue";
+// bootstrap
+import {createBootstrap} from "bootstrap-vue-next";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(createBootstrap());
+app.mount("#app");
